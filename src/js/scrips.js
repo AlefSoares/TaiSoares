f55bd142cb5926d5d4bbcae04e4d8ed61fb7d365
Alef Soares <alefcard.soares@gmail.com>
2025-03-20 09:51:20 -0300

    subindo o projeto

---
+++ src/js/scrips.js
@@ -0,0 +1,10 @@
+//HAMBURGER
+const hamburger = document.querySelector('.hamburger')
+const navUl = document.querySelector('.nav-ul')
+
+hamburger.addEventListener('click', function(){
+  hamburger.classList.toggle('active')
+  navUl.classList.toggle('active')
+  
+})
+
