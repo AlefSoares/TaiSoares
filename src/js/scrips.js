3a0320b140b163843567501ed481a52629dce5c1
Alef Soares <alefcard.soares@gmail.com>
2025-03-22 13:22:49 -0300

    Ajustes

--- src/js/scrips.js
+++ src/js/scrips.js
@@ -1,10 +1,19 @@
 //HAMBURGER
 const hamburger = document.querySelector('.hamburger')
 const navUl = document.querySelector('.nav-ul')
+const menuLinks = document.querySelectorAll(".nav-ul a");
 
 hamburger.addEventListener('click', function(){
   hamburger.classList.toggle('active')
   navUl.classList.toggle('active')
   
 })
+
+// Remove a classe que mantém o menu aberto
+menuLinks.forEach(link => {
+  link.addEventListener("click", function () {
+    navUl.classList.remove("active") 
+    hamburger.classList.remove('active')
+  });
+});
 
